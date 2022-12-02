d6f15845b3067166b95dfe5e7b765d1a278eafc0
dannylongeuay <dannylongeuay@yahoo.com>
2022-12-01 21:39:01 -0800

    feat(src): :sparkles: solve day 2 part 2

--- src/bin/2022_02.rs
+++ src/bin/2022_02.rs
@@ -11,7 +11,15 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines();
+    let mut score = 0;
+    for line in lines {
+        let mut chars = line.split(' ');
+        let opp_move = chars.next().unwrap();
+        let my_move = chars.next().unwrap();
+        score += score_round_2(opp_move, my_move)
+    }
+    Some(score)
 }
 
 fn score_round(a: &str, b: &str) -> u32 {
@@ -48,6 +56,40 @@ fn score_round(a: &str, b: &str) -> u32 {
     score
 }
 
+fn score_round_2(a: &str, b: &str) -> u32 {
+    let mut score = 0;
+
+    match b {
+        "X" => match a {
+            "A" => score += 3,
+            "B" => score += 1,
+            "C" => score += 2,
+            _ => {}
+        },
+        "Y" => {
+            score += 3;
+            match a {
+                "A" => score += 1,
+                "B" => score += 2,
+                "C" => score += 3,
+                _ => {}
+            }
+        }
+        "Z" => {
+            score += 6;
+            match a {
+                "A" => score += 2,
+                "B" => score += 3,
+                "C" => score += 1,
+                _ => {}
+            }
+        }
+        _ => {}
+    }
+
+    score
+}
+
 fn main() {
     let input = &advent_of_code::read_file("inputs", 2022, 2);
     advent_of_code::solve!(1, part_one, input);
@@ -67,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2022, 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
