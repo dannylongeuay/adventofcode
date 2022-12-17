a2561947075bd2f1c9562b6da59d588b7727dee9
Daniel Longeuay <dannylongeuay@yahoo.com>
2022-12-16 20:22:09 -0800

    feat(src): :sparkles: solve day 14

--- src/bin/2022_14.rs
+++ src/bin/2022_14.rs
@@ -37,7 +37,7 @@ impl Sub for Point {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Tile {
     EMPTY,
     ROCK,
@@ -53,7 +53,7 @@ struct Grid {
 }
 
 impl Grid {
-    fn new(input: &str) -> Self {
+    fn new(input: &str, infinite_floor: bool) -> Self {
         let mut x_min = i32::MAX;
         let mut x_max = i32::MIN;
         let mut y_max = i32::MIN;
@@ -93,14 +93,31 @@ impl Grid {
                 prev = Some(current_point);
             }
         }
-        let width = x_max - x_min + 1;
-        let height = y_max + 1;
-        let mut map: Vec<Tile> = vec![EMPTY; (width * height) as usize];
-        let sand_source = Point::new(500 - x_min, 0);
-        for rock_point in rock_points {
-            map[(rock_point.y * width + rock_point.x - x_min) as usize] = ROCK;
+        let mut width = x_max - x_min + 1;
+        let mut height = y_max + 1;
+        if infinite_floor {
+            width = x_max * 2;
+            height += 2;
         }
-        map[(sand_source.y * width + sand_source.x) as usize] = SAND;
+        let mut map: Vec<Tile> = vec![EMPTY; (width * height) as usize];
+        let mut sand_source = Point::new(500 - x_min, 0);
+        if infinite_floor {
+            sand_source = Point::new(500, 0);
+        }
+        for rock_point in rock_points {
+            let mut index = (rock_point.y * width + rock_point.x - x_min) as usize;
+            if infinite_floor {
+                index = (rock_point.y * width + rock_point.x) as usize;
+            }
+            map[index] = ROCK;
+        }
+        if infinite_floor {
+            map[(sand_source.y * width + sand_source.x) as usize] = SAND;
+            for x in 0..width {
+                let index = ((height - 1) * width + x) as usize;
+                map[index] = ROCK;
+            }
+        }
         Grid {
             map,
             width,
@@ -109,8 +126,58 @@ impl Grid {
         }
     }
 
-    fn get(&self, p: Point) -> Tile {
-        self.map[(p.y * self.width + p.x) as usize]
+    fn get(&self, p: &Point) -> Option<Tile> {
+        self.map.get((p.y * self.width + p.x) as usize).copied()
+    }
+
+    fn simulate(&mut self) {
+        let mut next_point = self.sand_source + Point::new(0, 1);
+
+        let mut safety = 0;
+        while let Some(tile) = self.get(&next_point) {
+            match tile {
+                EMPTY => {
+                    next_point = next_point + Point::new(0, 1);
+                }
+                _ => {
+                    let left_point = next_point + Point::new(-1, 0);
+                    let right_point = next_point + Point::new(1, 0);
+                    let left = self.get(&left_point);
+                    let right = self.get(&right_point);
+                    match (left, right) {
+                        // hit left edge of map, sand will fall endlessly
+                        (None, Some(_)) => {
+                            break;
+                        }
+                        // hit right edge of map, sand will fall endlessly
+                        (Some(_), None) => {
+                            break;
+                        }
+                        // left point is empty
+                        (Some(EMPTY), Some(_)) => {
+                            next_point = left_point;
+                        }
+                        // left is occupied and right point is empty
+                        (Some(_), Some(EMPTY)) => {
+                            next_point = right_point;
+                        }
+                        // left and right are occupied
+                        (Some(_), Some(_)) => {
+                            self.map[((next_point.y - 1) * self.width + next_point.x) as usize] =
+                                SAND;
+                            next_point = self.sand_source + Point::new(0, 1);
+                        }
+                        (None, None) => {
+                            unreachable!("both left and right points should not be None");
+                        }
+                    }
+                }
+            }
+            safety += 1;
+            if safety > 10_000_000 {
+                break;
+            }
+        }
     }
 }
 
@@ -118,9 +185,9 @@ impl Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for y in 0..self.height {
             for x in 0..self.width {
-                match self.get(Point::new(x, y)) {
+                match self.get(&Point::new(x, y)).unwrap() {
                     EMPTY => {
-                        write!(f, " ")?;
+                        write!(f, ".")?;
                     }
                     ROCK => {
                         write!(f, "#")?;
@@ -136,14 +203,18 @@ impl Display for Grid {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let grid = Grid::new(input);
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut grid = Grid::new(input, false);
+    grid.simulate();
     println!("{}", grid);
-    None
+    Some(grid.map.iter().filter(|&t| t == &SAND).count())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut grid = Grid::new(input, true);
+    grid.simulate();
+    // println!("{}", grid);
+    Some(grid.map.iter().filter(|&t| t == &SAND).count())
 }
 
 fn main() {
@@ -165,6 +236,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2022, 14);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(93));
     }
 }
